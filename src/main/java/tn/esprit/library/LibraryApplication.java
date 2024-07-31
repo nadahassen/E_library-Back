0481128f153546a467d78cb46524a2277d6e03f2
rayen-khalfaoui <rayen.khalfaoui@esprit.tn>
2024-07-31 11:09:01 +0200

    fixed port

--- src/main/java/tn/esprit/library/LibraryApplication.java
+++ src/main/java/tn/esprit/library/LibraryApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class
-LibraryApplication {
+public class LibraryApplication { // Fixed the class declaration formatting issue
 
     public static void main(String[] args) {
         SpringApplication.run(LibraryApplication.class, args);
